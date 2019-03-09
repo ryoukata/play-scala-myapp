44407d24ff4bd4554a73b643b1c97c0663bcb0f5
ryoya katagiri <ryoya3091@yahoo.co.jp>
2019-03-09 21:10:49 +0900

    form exercise add

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,23 +3,28 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
-import java.util._
+import play.api.data._
+import play.api.data.Forms._
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index("これはコントローラーで用意したメッセージです。"))
+  import forms.MyForm._
+
+  def index() = Action { implicit request =>
+    Ok(views.html.index(
+      "これはコントローラーで用意したメッセージです。",
+      myform
+    ))
   }
+
+  def form() = Action { implicit  request =>
+    val form = myForm.bindFromRequest
+    val data = form.get
+    Ok(views.html.index(
+      "name: " + data.name + ", pass: " + data.pass,
+      form
+    ))
+  }
+
 }
