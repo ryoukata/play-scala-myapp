2b4ef2cab15f2c61000b2fd7e5b5485f8af78cbc
ryoya katagiri <ryoya3091@yahoo.co.jp>
2019-03-16 17:59:12 +0900

    h2 database use

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,30 +1,45 @@
 package controllers
 
+import java.sql._
 import javax.inject._
 import play.api._
 import play.api.mvc._
 import play.api.data._
 import play.api.data.Forms._
+import play.api.db._
 
 @Singleton
-class HomeController @Inject()(cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
+class HomeController @Inject()(db: Database, cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
 
-  import forms.MyForm._
+  // import forms.MyForm._
 
   def index() = Action { implicit request =>
+    var msg = "database record:<br><ul>"
+    try {
+      db.withConnection { conn =>
+        val stmt = conn.createStatement
+        val rs = stmt.executeQuery("SELECT * FROM people")
+        while(rs.next){
+          msg += "<li>" + rs.getInt("id") + ":" + rs.getString("name") + "</li>"
+        }
+        msg += "</ul>"
+      }
+    } catch {
+      case e: SQLException =>
+        msg = "<li>no record...</li>"
+    }
     Ok(views.html.index(
-      "これはコントローラーで用意したメッセージです。",
-      myform
+      msg
     ))
   }
 
-  def form() = Action { implicit  request =>
+/*  def form() = Action { implicit  request =>
     val form = myform.bindFromRequest
     val data = form.get
     Ok(views.html.index(
       "name: " + data.name + ", pass: " + data.pass  + ", radio:" + data.radio,
       form
     ))
-  }
+  }*/
 
 }
--- build.sbt
+++ build.sbt
@@ -10,8 +10,8 @@ scalaVersion := "2.12.8"
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.1" % Test
 libraryDependencies += "com.h2database" % "h2" % "1.4.197"
-libraryDependencies += "evolutions"
-libraryDependencies += "jdbc"
+libraryDependencies += evolutions
+libraryDependencies += jdbc
 
 // Adds additional packages into Twirl
 //TwirlKeys.templateImports += "com.ryoya.play.controllers._"
