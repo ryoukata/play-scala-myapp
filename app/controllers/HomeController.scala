a8081449c78bce7b2c83ab23693f260cec68959e
ryoya katagiri <ryoya3091@yahoo.co.jp>
2019-03-18 00:14:05 +0900

    record create add

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -11,8 +11,6 @@ import play.api.db._
 @Singleton
 class HomeController @Inject()(db: Database, cc: MessagesControllerComponents) extends MessagesAbstractController(cc) {
 
-  // import forms.MyForm._
-
   def index() = Action { implicit request =>
     var msg = "database record:<br><ul>"
     try {
@@ -31,6 +29,37 @@ class HomeController @Inject()(db: Database, cc: MessagesControllerComponents) e
     Ok(views.html.index(
       msg
     ))
+  }
+
+  import forms.PersonForm._
+
+  // ユーザ登録画面の表示
+  def add() = Action { implicit request =>
+    Ok(views.html.add(
+      "フォームを入力してください。",
+      form
+    ))
+  }
+
+  def create() = Action { implicit request =>
+    val formdata = form.bindFromRequest
+    val data = formdata.get
+    try {
+      db.withConnection { conn =>
+        val ps = conn.prepareStatement("insert into people values (default, ?, ?, ?)")
+        ps.setString(1, data.name)
+        ps.setString(2, data.mail)
+        ps.setString(3, data.tel)
+        ps.executeUpdate
+      }
+    } catch {
+      case e: SQLException =>
+        Ok(views.html.add(
+          "フォームに入力してください。",
+          form
+        ))
+    }
+    Redirect(routes.HomeController.index)
   }
 
 /*  def form() = Action { implicit  request =>
